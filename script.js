3b50bf6f46a84716177ffb976b133353b3aa717b
alzeqri1997 <ahmed1221moh@gmail.com>
2022-11-03 22:15:26 +0300

    responsive website

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const select = (item) => document.querySelector(item);
+
 const activeButton = select('.active');
 const onButton = select('.on');
 const offButton = select('.off');
@@ -6,24 +7,15 @@ const navMobile = select('.nav');
 const header = select('.header');
 const menuIcon = select('.menu-btn');
 const closeIcon = select('.close-icon');
-
-
 const menuBtn = select('.menu-btn');
 
-console.log(menuBtn)
+
 
 function menuClick () {
   menuBtn.addEventListener("click", () => {
     header.classList.add('fixed-position')
     navMobile.classList.add('fixed-position')
     header.classList.toggle("shown");
-    // if (menuIcon.dataset.isMenuOpen) {
-    //   menuIcon.style.display = 'none'
-    //   closeIcon.style.display = 'inline-block'
-    // }else{
-    //   menuIcon.style.display = 'inline-block'
-    //   closeIcon.style.display = 'none'
-    // }
     console.log(select('.menu-icon').dataset.isMenuOpen)
   })
 }
@@ -42,3 +34,14 @@ function turnMusic(value) {
     offButton.style.color = '#fff'
   }
 }
+
+// Swiper
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
